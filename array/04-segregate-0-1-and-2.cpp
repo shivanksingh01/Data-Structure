11f1be5eb10d6badf6fa37eea084d157e2b59027
Shivank Singh <1906582@kiit.ac.in>
2022-05-21 02:23:05 +0530

    code updated

--- array/04-segregate-0-1-and-2.cpp
+++ array/04-segregate-0-1-and-2.cpp
@@ -7,6 +7,26 @@
 using namespace std;
 
 
+// Naive approach
+//  count number of 0's, 1's and 2's
+// overwrite original array
+void segregate012(int arr[], int n){
+    int count0 = 0, count1 = 0, count2 = 0;
+    for (int i = 0; i < n; i++){
+        if (arr[i] == 0)
+            count0++;       // count 0's
+        else if (arr[i] == 1)
+            count1++;       // count 1's
+        else if (arr[i] == 2)
+            count2++;       // count 2's
+    }
+    for (int i = 0; i < count0; i++)
+        arr[i] = 0;         // overwrite array with 0
+    for (int i = count0; i < count1 + count0; i++)
+        arr[i] = 1;         // overwrite array with 1
+    for (int i = count1 + count0; i < count1 + count2 + count0; i++)
+        arr[i] = 2;         // overwrite array with 2
+}
 
 // We can rearrange the array in a single traversal using an alternative linear-time partition routine that separates the values into three groups:
 // The values less than the pivot,   //
--- array/05-segregate-negative-and-positive.cpp
+++ array/05-segregate-negative-and-positive.cpp
@@ -1,17 +1,18 @@
 // segregate positive and negative elemnts in an array
 // Move all negative elemnts to one side of array
 // Input  :  12  11  -13  -5  6  -7  5  -3  -6
-// Output : -13  -5  -7  -3  -6  12  11  6  5  
+// Output : -13  -5  -7  -3  -6  12  11  6  5
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 
+// Approach-1
 void segregatePosNeg(int arr[], int n){
     int j = -1;
     int pivot = 0;
     for(int i=0;i<n;i++){
         if(arr[i]< pivot){
-            j++;  
+            j++;
             // j will keep track of last negative elemnt in segregated array
             // swap(arr[i], arr[j])
             int temp=arr[i];
@@ -21,6 +22,36 @@ void segregatePosNeg(int arr[], int n){
     }
 }
 
+// Approach 2 Dutch-flag algorithm
+void segregatePosNeg(int arr[], int n){
+    int low=0;
+    int high=n-1;
+    while(low<high){
+        while(arr[low]>0){
+            low++;
+        }
+        while(arr[high]<0){
+            high--;
+        }
+
+        if(low<high){
+            swap(arr[low], arr[high]);
+        }
+    }
+}
+
+// Approach-3 (Taking pivot element) partitioning logic of Quicksort
+void segregatePosNeg(int arr[], int n){
+    int pivot = 0; // each time we find a negative number, `pivot` is incremented,
+    // and that element would be placed before the pivot
+    // poivot points to next index before which all elemnts are negative
+    for (int i = 0; i < n; i++){
+        if (arr[i] < 0){
+            swap(arr[pivot], arr[i]);
+            pivot++;
+        }
+    }
+}
 
 void display(int arr[], int n){
     for(int i=0;i<n;i++){
@@ -31,7 +62,7 @@ void display(int arr[], int n){
 
 int main(){
     int arr[] = {2, -1, 4, 5, -8, 6, -11, 10};
-    int n = sizeof(arr)/sizeof(n);   
+    int n = sizeof(arr)/sizeof(n);
     display(arr, n);
     segregatePosNeg(arr, n);
     display(arr,n);
