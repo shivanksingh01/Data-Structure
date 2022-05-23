d3ef8e68b9243736f24f7e0e2d88cc6bd3501a3c
Shivank Singh <1906582@kiit.ac.in>
2022-05-23 22:47:51 +0530

    code update

---
+++ binary-search/13-minimum-diffrence-element.cpp
@@ -0,0 +1,22 @@
+// Find minimum diffrence elemnt in an sorted array
+// Given an array of integers sorted in ascending order, and a target value
+// find the element in the array that has minimum difference with the target value.
+
+// Input :       [ 2,   5,   10,   12,   15 ], target = 8 , output = 10
+// explanation : [8-2, 8-5, 10-8, 12-8, 15-8]
+//               [ 6,   3,   2,    4,    7]
+//  output  : 10  , 10 has min diffrence with the given target
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int minDiffrence(int arr[], int n){
+
+}
+
+int main(){
+    int arr[] = {1, 5 };
+    int n = sizeof(arr)/sizeof(int);
+    int minDiffrence(arr, n);
+    return 0;
+}
