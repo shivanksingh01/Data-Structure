aaa5fbec3f20f27cead83197aa31aec275625f12
Shivank Singh <1906582@kiit.ac.in>
2022-05-25 15:47:02 +0530

    code updated

--- heap/0-heap-sort.c
+++ heap/0-heap-sort.c
@@ -2,8 +2,7 @@
 
 #include <stdio.h>
 
-void swap(int *a, int *b)
-{
+void swap(int *a, int *b){
     int temp = *a;
     *a = *b;
     *b = temp;
@@ -11,8 +10,7 @@ void swap(int *a, int *b)
 
 // To heapify a subtree rooted with node i which is an index in arr[].
 //  n is the size of heap /(arr)
-void heapify(int arr[], int n, int i)
-{
+void heapify(int arr[], int n, int i){
     int max = i;
     int left = 2 * i + 1;
     int right = 2 * i + 2;
@@ -25,21 +23,18 @@ void heapify(int arr[], int n, int i)
 //     left < n --> index of left child is in bounds of array (i.e. not exceeding the array )
 //     right < n --> index of right child is in bounds of array (i.e. not exceeding the array )  : within the rannge
 
-    if (max != i)   // if largest is not root
-    {
-        swap(&arr[i], &arr[max]);   
+    if (max != i){   // if largest is not root
+        swap(&arr[i], &arr[max]);
         //recurrsively heapify the affected sub-tree
         heapify(arr, n, max);
     }
 }
 
 
-void heapsort(int arr[], int n)
-{
+void heapsort(int arr[], int n){
     for (int i = n / 2 - 1; i >= 0; i--)  // build heap
         heapify(arr, n, i);
-    for (int i = n - 1; i >= 0; i--)  //extract elemnt from heap one-by-one
-    {
+    for (int i = n - 1; i >= 0; i--){  //extract elemnt from heap one-by-one
         swap(&arr[0], &arr[i]);   // move rpot to end
         // call max heapify on the reduced heap
         heapify(arr, i, 0);
@@ -48,8 +43,7 @@ void heapsort(int arr[], int n)
 
 // display the array of size n
 void display(int arr[], int n){
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         printf("%d\t", arr[i]);
     }
     printf("\n");
@@ -59,7 +53,7 @@ void display(int arr[], int n){
 int main(){
     int arr[] = { 12, 11, 13, 5, 6, 7, 15, 10 };
     int n = sizeof(arr) / sizeof(arr[0]);
- 
+
     heapsort(arr, n);
 
     printf("sorted array : \n");
